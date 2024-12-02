8a280a489449d92b205d6385dd4819bf2cbff87f
01001100Trinh10 <luketrinh101@gmail.com>
2024-12-02 15:28:52 -0800

    Update client.py
    comments and documentation

--- client.py
+++ client.py
@@ -15,7 +15,7 @@ while True:
             myTCPSocket.connect((serverIP, serverPort)) 
 
             while True:
-                #user inputs a message to send
+                #user inputs from one of the queries
                 someData = str(input("Enter a number to enter the following queries:\n1. What is the average moisture inside my kitchen fridge in the past three hours?\n2. What is the average water consumption per cycle in my smart dishwasher?\n3. Which device consumed more electricity among my three IoT devices (two refrigerators and a dishwasher)?"))
                 if someData == "1" and someData == "2" and someData == "3":
                     myTCPSocket.send(bytearray(str(someData), encoding='utf-8'))
@@ -23,10 +23,11 @@ while True:
                     serverResponse = myTCPSocket.recv(1024)
 
                     print(serverResponse.decode('utf-8'))
-                    #allows the user to send multiple messages
+                    #allows the user to send multiple queries
                     condition = input("do you want to exit, type y/n: ")
                     if condition.lower() == "y":
                         break
+                #sends a user-friendly message in case the user enters the wrong value
                 else:
                     print("Sorry, this query cannot be processed. Try entering a number from one of the 3 queries")
             myTCPSocket.close() #closes the socket

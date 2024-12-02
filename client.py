d7ec659cd03d9029e804a31688a40a93a1fc2bc5
01001100Trinh10 <luketrinh101@gmail.com>
2024-12-02 15:20:21 -0800

    Update client.py
    Prompts the user to enter one of the 3 queries. Has the user try again if they entered the wrong values.

--- client.py
+++ client.py
@@ -9,7 +9,7 @@ while True:
         serverPort= int(input("enter the server port number:"))
 
         try:
-            #creats a tcp socket
+            #creates a tcp socket
             myTCPSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             #connects to the server
             myTCPSocket.connect((serverIP, serverPort)) 
@@ -17,15 +17,18 @@ while True:
             while True:
                 #user inputs a message to send
                 someData = str(input("Enter a number to enter the following queries:\n1. What is the average moisture inside my kitchen fridge in the past three hours?\n2. What is the average water consumption per cycle in my smart dishwasher?\n3. Which device consumed more electricity among my three IoT devices (two refrigerators and a dishwasher)?"))
-                myTCPSocket.send(bytearray(str(someData), encoding='utf-8'))
-                #client recieves a message back
-                serverResponse = myTCPSocket.recv(1024)
+                if someData == "1" and someData == "2" and someData == "3":
+                    myTCPSocket.send(bytearray(str(someData), encoding='utf-8'))
+                    #client recieves a message back
+                    serverResponse = myTCPSocket.recv(1024)
 
-                print(serverResponse.decode('utf-8'))
-                #allows the user to send multiple messages
-                condition = input("do you want to exit, type y/n: ")
-                if condition.lower() == "y":
-                    break
+                    print(serverResponse.decode('utf-8'))
+                    #allows the user to send multiple messages
+                    condition = input("do you want to exit, type y/n: ")
+                    if condition.lower() == "y":
+                        break
+                else:
+                    print("Sorry, this query cannot be processed. Try entering a number from one of the 3 queries")
             myTCPSocket.close() #closes the socket
             break
         #checks if the client is able to connect

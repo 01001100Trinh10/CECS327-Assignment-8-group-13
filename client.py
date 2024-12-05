da8a230c2d8a45884093b957df4a74db11cdee9d
01001100Trinh10 <luketrinh101@gmail.com>
2024-12-04 23:27:45 -0800

    Adjusted input
    Allow a 4th option for the user to exit. Makes it so that you don't need to have a valid input in order to exit the client/server.

--- client.py
+++ client.py
@@ -16,16 +16,14 @@ while True:
 
             while True:
                 #user inputs from one of the queries
-                someData = str(input("Enter a number to enter the following queries:\n1. What is the average moisture inside my kitchen fridge in the past three hours?\n2. What is the average water consumption per cycle in my smart dishwasher?\n3. Which device consumed more electricity among my three IoT devices (two refrigerators and a dishwasher)?"))
+                someData = str(input("Enter a number to enter the following queries:\n1. What is the average moisture inside my kitchen fridge in the past three hours?\n2. What is the average water consumption per cycle in my smart dishwasher?\n3. Which device consumed more electricity among my three IoT devices (two refrigerators and a dishwasher)?\n4. Exit"))
                 if someData == "1" or someData == "2" or someData == "3":
                     myTCPSocket.send(bytearray(str(someData), encoding='utf-8'))
                     #client recveis a message back
                     serverResponse = myTCPSocket.recv(1024)
                     print(serverResponse.decode('utf-8'))
-                    #allows the user to send multiple queries
-                    condition = input("do you want to exit, type y/n: ")
-                    if condition.lower() == "y":
-                        break
+                if someData == "4":
+                    break
                 #sends a user-friendly message in case the user enters the wrong value
                 else:
                     print("Sorry, this query cannot be processed. Try entering a number from one of the 3 queries")
